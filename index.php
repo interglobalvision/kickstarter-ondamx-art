f089102f697aa28417db0896fae8d4f6e86f32d9
mikeyrayvon <mikey@interglobal.vision>
2018-11-11 16:16:32 -0600

    scaffold index

--- index.php
+++ index.php
@@ -3,37 +3,45 @@ get_header();
 ?>
 
 <main id="main-content">
-  <section id="posts">
+  <section id="rewards">
     <div class="container">
-      <div class="grid-row">
+      <div class="masonry-wrapper">
 
 <?php
 if (have_posts()) {
   while (have_posts()) {
     the_post();
+
+    $title = get_post_meta($post->ID, '_igv_work_title', true);
+    $price_mxn = get_post_meta($post->ID, '_igv_work_price_mxn', true);
+    $price_usd = get_post_meta($post->ID, '_igv_work_price_usd', true);
+    $year = get_post_meta($post->ID, '_igv_work_year', true);
 ?>
 
-        <article <?php post_class('grid-item item-s-12'); ?> id="post-<?php the_ID(); ?>">
+        <article <?php post_class('masonry-item item-s-6 item-m-4 item-xl-3'); ?> id="post-<?php the_ID(); ?>">
 
-          <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+          <a href="<?php the_permalink() ?>">
+            <div>
+              <?php the_post_thumbnail(); ?>
+            </div>
+            <div>
+              <span><?php echo !empty($price_mxn) ? '$' . $price_mxn . ' MXN' : ''; ?></span>
+              <span><?php echo !empty($price_usd) ? '$' . $price_usd . ' USD' : ''; ?></span>
+            </div>
+            <h2 class="u-inline font-italic"><?php echo !empty($title) ? $title : ''; ?></h2>, <span><?php echo !empty($year) ? $year : ''; ?></span>
 
-          <?php the_content(); ?>
+          </a>
 
         </article>
 
 <?php
   }
-} else {
+}
 ?>
-        <article class="u-alert grid-item item-s-12"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
-<?php
-} ?>
 
       </div>
     </div>
   </section>
-
-  <?php get_template_part('partials/pagination'); ?>
 
 </main>
 
