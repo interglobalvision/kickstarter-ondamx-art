7f9ba99c8f6e6b0982d825b8cca0e7c942f873c9
mikeyrayvon <mikey@interglobal.vision>
2018-11-11 20:33:56 -0600

    disable post thumb lazyload

--- index.php
+++ index.php
@@ -24,7 +24,11 @@ if (have_posts()) {
 
           <a href="<?php the_permalink() ?>">
             <div class="thumb-holder margin-bottom-tiny">
-              <?php the_post_thumbnail('catalog-thumb'); ?>
+              <?php
+                $post_thumbnail_id = get_post_thumbnail_id( $post->ID );
+
+                echo !empty($post_thumbnail_id) ? wp_get_attachment_image($post_thumbnail_id, 'catalog-thumb', false, array('data-no-lazysizes'=>'true')) : '';
+              ?>
             </div>
             <h2 class="font-size-mid"><?php echo !empty($artist) ? $artist : ''; ?></h2>
             <div>
