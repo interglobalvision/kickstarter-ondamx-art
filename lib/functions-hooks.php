08b08dd79b04f8b2644db6a9713db798252b12db
mikeyrayvon <mikey@interglobal.vision>
2018-11-11 20:34:54 -0600

    remove post order

--- lib/functions-hooks.php
+++ lib/functions-hooks.php
@@ -1,11 +1,3 @@
 <?php
 
 // Custom hooks (like excerpt length etc)
-
-function order_posts_by_title( $query ) {
-  if ( $query->is_home() && $query->is_main_query() ) {
-    $query->set( 'orderby', 'title' );
-    $query->set( 'order', 'ASC' );
-  }
-}
-add_action( 'pre_get_posts', 'order_posts_by_title' );
