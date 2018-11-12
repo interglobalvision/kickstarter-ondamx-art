a419a8f28c5d0aae90157e02aecf82563c55f357
mikeyrayvon <mikey@interglobal.vision>
2018-11-11 18:46:24 -0600

    scaffold intro partial

---
+++ partials/intro.php
@@ -0,0 +1,13 @@
+<?php
+$options = get_site_option('_igv_custom_options');
+
+if (!empty($options['intro_text'])) {
+?>
+<section id="intro" class="text-align-center grid-row justify-center padding-top-basic margin-bottom-large">
+  <div class="grid-item item-s-12 item-m-10 item-l-8 font-size-large">
+    <?php echo apply_filters('the_content', $options['intro_text']); ?>
+  </div>
+</section>
+<?php
+}
+?>
