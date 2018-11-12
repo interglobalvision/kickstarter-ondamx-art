ec2461c6ae3b5d22c992ce7a99d1879ed6d034a6
mikeyrayvon <mikey@interglobal.vision>
2018-11-11 19:20:01 -0600

    fix widows in intro

--- partials/intro.php
+++ partials/intro.php
@@ -3,7 +3,7 @@ $options = get_site_option('_igv_custom_options');
 
 if (!empty($options['intro_text'])) {
 ?>
-<section id="intro" class="text-align-center grid-row justify-center padding-top-basic margin-bottom-large">
+<section id="intro" class="text-align-center grid-row justify-center padding-top-basic margin-bottom-large js-fix-widows">
   <div class="grid-item item-s-12 item-m-10 item-l-8 font-size-large">
     <?php echo apply_filters('the_content', $options['intro_text']); ?>
   </div>
