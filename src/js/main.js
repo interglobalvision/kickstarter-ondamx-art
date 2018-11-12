d691eac842fc73b40dbb0e42d26f3d8a4ebb017d
mikeyrayvon <mikey@interglobal.vision>
2018-11-11 18:47:39 -0600

    masonry and marquee config

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,9 @@
 
 // Import dependencies
 import lazySizes from 'lazysizes';
+import Masonry from 'masonry-layout';
+import marquee from 'jquery.marquee';
+import imagesLoaded from 'imagesloaded';
 
 // Import style
 import '../styl/site.styl';
@@ -18,11 +21,23 @@ class Site {
   }
 
   onResize() {
+    this.layoutMasonry();
 
   }
 
   onReady() {
     lazySizes.init();
+
+    this.$window = $(window);
+
+    this.windowHeight = this.$window.height();
+    this.windowWidth = this.$window.width();
+    this.masonryImagesLoaded = false;
+
+    this.$masonryHolder = $('#masonry-holder');
+
+    this.initMasonry();
+    this.initMarquee();
 
   }
 
@@ -33,6 +48,57 @@ class Site {
       string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
       $(this).html(string);
     });
+  }
+
+  initMasonry() {
+    var _this = this;
+
+    if (_this.$masonryHolder.length) {
+
+      _this.masonryInstance = new Masonry( '#masonry-holder', {
+        itemSelector: '.masonry-item',
+        transitionDuration: 0,
+        initLayout: false,
+        percentPosition: true
+      });
+
+      _this.masonryInstance.layout();
+      _this.$masonryHolder.removeClass('hidden');
+
+      imagesLoaded('#masonry-holder').on( 'progress', function(imgLoad, image) {
+        _this.masonryInstance.layout();
+      });
+
+    }
+  }
+
+  layoutMasonry() {
+    var _this = this;
+
+    if (_this.$masonryHolder.length) {
+      _this.masonryInstance.layout();
+    }
+  }
+
+  initMarquee() {
+    const $marquee = $('#marquee');
+
+    if ($marquee.length) {
+      $marquee.marquee({
+      	//duration in milliseconds of the marquee
+      	duration: 15000,
+      	//gap in pixels between the tickers
+      	gap: 50,
+      	//time in milliseconds before the marquee will start animating
+      	delayBeforeStart: 0,
+      	//'left' or 'right'
+      	direction: 'left',
+      	//true or false - should the marquee be duplicated to show an effect of continues flow
+      	duplicated: false
+      });
+
+      $marquee.addClass('show');
+    }
   }
 }
 
