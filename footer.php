a3390a1a25b8c95a012ab0869c33669c8c2eb3d1
mikeyrayvon <mikey@interglobal.vision>
2018-11-11 21:05:46 -0600

    smaller credit

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
   <footer id="footer">
     <div class="container grid-row justify-center padding-bottom-small padding-top-small">
-      <div class="grid-item">
+      <div class="grid-item font-size-small">
         Hecho por <a class="link-underline" href="https://interglobal.vision">Interglobal.Vision</a> en la CDMX
       </div>
     </div>
